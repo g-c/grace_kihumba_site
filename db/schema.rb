76d1e4f87121cf02eba14535a2fd4473ab88c723
Grace Kihumba <gkihumba@gmail.com>
2013-09-08 13:43:08 -0400

    Make User model with authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130908172046) do
+ActiveRecord::Schema.define(version: 20130908174144) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20130908172046) do
     t.datetime "updated_at"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
